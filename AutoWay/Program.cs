1d45b6edc93b2badb055ede6643ad7f0733cd00e
LA246517-SBAI <la246517@student.helha.be>
2025-10-06 10:25:16 +0200

    Ajout des fichiers de configuration

--- AutoWay/Program.cs
+++ AutoWay/Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AutoWay.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AutoWayContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AutoWayContext")
+        ?? throw new InvalidOperationException("Connection string 'AutoWayContext' not found.")));
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
