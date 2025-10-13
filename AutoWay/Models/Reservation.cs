61546540aef669119ea181fe046c26c2d7118929
momomob111 <la246591@student.helha.be>
2025-10-13 16:45:01 +0200

    ajout de l'attribut userID

--- AutoWay/Models/Reservation.cs
+++ AutoWay/Models/Reservation.cs
@@ -2,5 +2,11 @@
 {
     public class Reservation
     {
+        public int ReservationID { get; set; }
+        public DateTime DateDebut { get; set; }
+        public DateTime DateFin { get; set; }
+        public double PrixFinal { get; set; }
+
+        public int UtilisateurID { get; set; }
     }
 }
