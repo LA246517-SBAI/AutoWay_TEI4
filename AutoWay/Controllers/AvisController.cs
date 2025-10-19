c03fb00cfbb37abf7ba14feadcb46f13672a76d9
LA236333-Arifi <la236333@student.helha.be>
2025-10-19 18:48:57 +0200

    Ajout du controlleur pour les avis. Le program.cs n'a pas l'air adapté pour le controlleur MVC choisi

---
+++ AutoWay/Controllers/AvisController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AutoWay.Data;
+using AutoWay.Models;
+
+namespace AutoWay.Controllers
+{
+    public class AvisController : Controller
+    {
+        private readonly AutoWayContext _context;
+
+        public AvisController(AutoWayContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Avis
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Avis.ToListAsync());
+        }
+
+        // GET: Avis/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avis = await _context.Avis
+                .FirstOrDefaultAsync(m => m.AvisID == id);
+            if (avis == null)
+            {
+                return NotFound();
+            }
+
+            return View(avis);
+        }
+
+        // GET: Avis/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Avis/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AvisID,AvisMessage,AvisScore,AvisDate,UtilisateurID")] Avis avis)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(avis);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(avis);
+        }
+
+        // GET: Avis/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avis = await _context.Avis.FindAsync(id);
+            if (avis == null)
+            {
+                return NotFound();
+            }
+            return View(avis);
+        }
+
+        // POST: Avis/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("AvisID,AvisMessage,AvisScore,AvisDate,UtilisateurID")] Avis avis)
+        {
+            if (id != avis.AvisID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(avis);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AvisExists(avis.AvisID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(avis);
+        }
+
+        // GET: Avis/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var avis = await _context.Avis
+                .FirstOrDefaultAsync(m => m.AvisID == id);
+            if (avis == null)
+            {
+                return NotFound();
+            }
+
+            return View(avis);
+        }
+
+        // POST: Avis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var avis = await _context.Avis.FindAsync(id);
+            if (avis != null)
+            {
+                _context.Avis.Remove(avis);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AvisExists(int id)
+        {
+            return _context.Avis.Any(e => e.AvisID == id);
+        }
+    }
+}
