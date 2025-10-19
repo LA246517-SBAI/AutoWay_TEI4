3b1d02b08fec7a0f0d24c33f99c505c4111289bf
LA236333-Arifi <la236333@student.helha.be>
2025-10-19 19:19:53 +0200

    Mise à jour du controlleur en utilisant la classe parent ControllerBase pour la conception de l'API

--- AutoWay/Controllers/AvisController.cs
+++ AutoWay/Controllers/AvisController.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AutoWay.Data;
 using AutoWay.Models;
 
 namespace AutoWay.Controllers
 {
-    public class AvisController : Controller
+    [Route("[controller]")]
+    [ApiController]
+    public class AvisController : ControllerBase
     {
         private readonly AutoWayContext _context;
 
@@ -19,134 +21,83 @@ namespace AutoWay.Controllers
             _context = context;
         }
 
-        // GET: Avis
-        public async Task<IActionResult> Index()
+        // GET: api/Avis
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Avis>>> GetAvis()
         {
-            return View(await _context.Avis.ToListAsync());
+            return await _context.Avis.ToListAsync();
         }
 
-        // GET: Avis/Details/5
-        public async Task<IActionResult> Details(int? id)
+        // GET: api/Avis/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Avis>> GetAvis(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var avis = await _context.Avis
-                .FirstOrDefaultAsync(m => m.AvisID == id);
-            if (avis == null)
-            {
-                return NotFound();
-            }
-
-            return View(avis);
-        }
-
-        // GET: Avis/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Avis/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("AvisID,AvisMessage,AvisScore,AvisDate,UtilisateurID")] Avis avis)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(avis);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(avis);
-        }
-
-        // GET: Avis/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             var avis = await _context.Avis.FindAsync(id);
+
             if (avis == null)
             {
                 return NotFound();
             }
-            return View(avis);
+
+            return avis;
         }
 
-        // POST: Avis/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("AvisID,AvisMessage,AvisScore,AvisDate,UtilisateurID")] Avis avis)
+        // PUT: api/Avis/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAvis(int id, Avis avis)
         {
             if (id != avis.AvisID)
             {
-                return NotFound();
+                return BadRequest();
             }
 
-            if (ModelState.IsValid)
+            _context.Entry(avis).State = EntityState.Modified;
+
+            try
             {
-                try
-                {
-                    _context.Update(avis);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AvisExists(avis.AvisID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                await _context.SaveChangesAsync();
             }
-            return View(avis);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AvisExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // GET: Avis/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        // POST: api/Avis
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Avis>> PostAvis(Avis avis)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            _context.Avis.Add(avis);
+            await _context.SaveChangesAsync();
 
-            var avis = await _context.Avis
-                .FirstOrDefaultAsync(m => m.AvisID == id);
+            return CreatedAtAction("GetAvis", new { id = avis.AvisID }, avis);
+        }
+
+        // DELETE: api/Avis/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAvis(int id)
+        {
+            var avis = await _context.Avis.FindAsync(id);
             if (avis == null)
             {
                 return NotFound();
             }
 
-            return View(avis);
-        }
-
-        // POST: Avis/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var avis = await _context.Avis.FindAsync(id);
-            if (avis != null)
-            {
-                _context.Avis.Remove(avis);
-            }
-
+            _context.Avis.Remove(avis);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return NoContent();
         }
 
         private bool AvisExists(int id)
