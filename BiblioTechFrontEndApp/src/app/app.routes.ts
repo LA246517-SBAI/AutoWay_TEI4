fab42e1ec7bf6ee82e5f612025a7c2269bb57cd5
momomob111 <la246591@student.helha.be>
2025-11-17 14:10:13 +0100

    Ajout de angular material

--- BiblioTechFrontEndApp/src/app/app.routes.ts
+++ BiblioTechFrontEndApp/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ConnexionComponent } from './components/connexion/connexion.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'connexion', component: ConnexionComponent},
+];
--- BiblioTechFrontEndApp/src/app/components/connexion/connexion.component.ts
+++ BiblioTechFrontEndApp/src/app/components/connexion/connexion.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-connexion',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule,MatIconModule],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.css'
 })
 export class ConnexionComponent {
-
+hide = true;
 }
