890d8168d31927e41a09622474a2d0f9425b0902
momomob111 <la246591@student.helha.be>
2025-11-17 15:39:46 +0100

    mise à jour du css de connexion

--- BiblioTechFrontEndApp/src/app/components/connexion/connexion.component.ts
+++ BiblioTechFrontEndApp/src/app/components/connexion/connexion.component.ts
@@ -3,12 +3,13 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-connexion',
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule,MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule,MatIconModule,HeaderComponent],
   templateUrl: './connexion.component.html',
-  styleUrl: './connexion.component.css'
+  styleUrls: ['./connexion.component.css']
 })
 export class ConnexionComponent {
 hide = true;
