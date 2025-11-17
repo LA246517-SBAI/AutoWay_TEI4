683f12e50a473bfe4c8e9531864bdd2e0663d8a5
LA235837-Benkahla <la235837@student.helha.be>
2025-11-17 14:53:18 +0100

    Ajout de l'interface Livre

---
+++ BiblioTechFrontEndApp/src/app/details-livre/details-livre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-livre',
+  imports: [],
+  templateUrl: './details-livre.component.html',
+  styleUrl: './details-livre.component.css'
+})
+export class DetailsLivreComponent {
+
+}
---
+++ BiblioTechFrontEndApp/src/app/form-livre/form-livre.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-livre',
+  imports: [],
+  templateUrl: './form-livre.component.html',
+  styleUrl: './form-livre.component.css'
+})
+export class FormLivreComponent {
+
+}
