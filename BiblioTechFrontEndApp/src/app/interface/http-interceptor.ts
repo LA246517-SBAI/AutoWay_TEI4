827a0b840b81fe40c2cef5c9930e372a422a9b3e
LA235837-Benkahla <la235837@student.helha.be>
2025-12-14 22:44:02 +0100

    correction du merge

--- BiblioTechFrontEndApp/src/app/interface/http-interceptor.ts
+++ BiblioTechFrontEndApp/src/app/interface/http-interceptor.ts
@@ -1,12 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem("token"); // ton token JWT
+    if (!this.isBrowser) {
+      return next.handle(req);
+    }
+
+    const token = localStorage.getItem("token");
 
     if (token) {
       req = req.clone({
