8f6485ec92494e629745c7f20f5868ade5558b31
la236333 <la236333@student.helha.be>
2025-12-15 10:19:43 +0100

    Ajout des DTOs pour avoir les réponses appropriées au frontend

---
+++ BiblioTech/BibliothequeAPI/Dtos/CategorieDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BibliothequeAPI.Dtos
+{
+    public class CategorieDto
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; } = string.Empty;
+    }
+}
---
+++ BiblioTech/BibliothequeAPI/Dtos/EmpruntDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BibliothequeAPI.Dtos
+{
+    public class EmpruntDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int LivreId { get; set; }
+        public DateTime DateEmprunt { get; set; }
+        public DateTime DateRetourPrevue { get; set; }
+        public DateTime? DateRetourEffective { get; set; }
+        public LivreDto? Livre { get; set; }
+    }
+}
