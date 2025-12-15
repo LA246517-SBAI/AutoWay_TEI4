bcba54bc2872f938ac2180e27893ae6d27e726b1
LA246517-SBAI <la246517@student.helha.be>
2025-12-15 09:23:15 +0100

    Modification du JWT

--- BiblioTechFrontEndApp/src/app/app.config.ts
+++ BiblioTechFrontEndApp/src/app/app.config.ts
@@ -1,9 +1,11 @@
+// app.config.ts
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtInterceptor } from './interface/http-interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),  
     provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    provideAnimations(),
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ]
 };
